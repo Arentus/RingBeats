603f0874a95104aecd5875b8931b252656a535f0
Robwert Mota <robwert7991@gmail.com>
2018-08-11 17:05:18 -0400

    Initial commit

---
+++ includes/main-section.php
@@ -0,0 +1,25 @@
+<main>
+      <section>
+        <div class="container-fluid newsdeploy">
+            <div class="row no-gutters">
+              <div class="col-md-6">
+                <a href="guitars"><img class="img-fluid" src="assets/img/Best-Beginner-Electric-Guita.jpg"></a>
+
+                <span><h3 class="photo-text">Guitars on the House</h3></span>
+              </div>
+              <div class="col-md-6">
+                <div class="col-md-12">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididu
+                </div>
+                <div class="col-md-12">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud ex
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+    </main>
+    
---
+++ includes/nav.php
@@ -0,0 +1,33 @@
+
+    <header>
+
+      <nav class="navbar navbar-expand-md navdwrap ">
+        <a class="navbar-brand" href="#">RingBeats</a>
+        <button class="navbar-toggler navbluebtn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon navblackicon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="caracteristicas">Caracteristicas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Guitarras</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="#">Bajos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cuatros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Banjos</a>
+            </li>
+          </ul>
+        </div>
+        </nav>
+    </header>
+  
